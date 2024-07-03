237a9173809c7665fdbdb47cff9390bd0319978f
Facundo Lentini <lentinifacundo@gmail.com4>
2024-07-04 01:01:35 +1000

    CORS y methods

--- api/server.js
+++ api/server.js
@@ -1,21 +1,15 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const path = require('path')
-const fs = require('fs')
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db)
-const cors = require('cors')
 
 server.use(middlewares)
 // Add this before server.use(router)
-/* server.use(jsonServer.rewriter({
+server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/product/:resource/:id/show': '/:resource/:id'
-})) */
-
-server.use(cors())
+}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
