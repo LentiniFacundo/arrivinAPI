bd1d3ad322527fb555952d9c8917b4809010392a
Facundo Lentini <lentinifacundo@gmail.com4>
2024-07-07 22:51:44 +1000

    Corrigiendo nombre item 6

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/product/:resource/:id/show': '/:resource/:id'
@@ -15,5 +13,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
